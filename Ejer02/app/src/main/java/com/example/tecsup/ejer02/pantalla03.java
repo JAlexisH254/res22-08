dd575dd6e98e58e018d84a601eeb898c130a647a
JAlexisH254 <atarashi.23@gmail.com>
2019-08-26 21:50:53 -0500

    nuevo 26-08

---
+++ Ejer02/app/src/main/java/com/example/tecsup/ejer02/pantalla03.java
@@ -0,0 +1,13 @@
+package com.example.tecsup.ejer02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pantalla03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla03);
+    }
+}
